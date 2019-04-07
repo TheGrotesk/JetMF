8a342f0152940f96ad3e129898ba74f765ea7e3f
Indie Alex <sashapoberezhnyjj0@gmail.com>
2019-04-07 11:59:28 +0300

    update

--- core/welcomePage.php
+++ core/welcomePage.php
@@ -6,7 +6,7 @@
   </head>
   <body style="padding:20px;">
       <h1>Jet Micro Framework</h1>
-      <p>Welcome to Jet! Your application work fine!</p>
+      <p>Welcome to Jet! Your application works fine!</p>
       <h5>Links</h5>
       <a href="https://github.com/TheGrotesk/JetMF">GitHub</a>
       <a href="https://packagist.org/packages/jet-mf/jet-micro-framework">Packagist</a>
