b13bd2030d4e93cde298beeb97f8030ad2e582a0
Indie Alex <sashapoberezhnyjj0@gmail.com>
2019-04-07 12:23:03 +0300

    core refactoring

--- core/Application.php
+++ core/Application.php
@@ -26,21 +26,7 @@
           function render($class, $view)
           {
               if(array_key_exists('baseTemplate', $this->settings))
-              {
-                if(array_key_exists('folderControllers', $this->settings))
-                {
-                  if(file_exists($this->settings['folderControllers'].$class) && file_exists($this->settings['baseTemplate']))
-                  {
-                    require_once($this->settings['folderControllers'].$class);
-                    require_once($this->settings['baseTemplate']);
-                  }else
-                  {
-                    throw new \Exception("Class or view not found (base)", 1);
-
-                  }
-                }
-                else
-                {
+              {            
                   if(file_exists($class) && file_exists($this->settings['baseTemplate']))
                   {
                     require_once($class);
@@ -51,45 +37,18 @@
                     throw new \Exception("Class or view not found (base)", 1);
 
                   }
-                }
 
               }else
               {
-                if(array_key_exists('folderControllers', $this->settings))
-                {
-                  if(file_exists($this->settings['folderControllers'].$class) && file_exists($view))
+                  if(file_exists($class) && file_exists($view))
                   {
-                    require_once($this->settings['folderControllers'].$class);
+                    require_once($class);
                     require_once($view);
                   }else
                   {
                     throw new \Exception("Class or view not found (custom)", 1);
                   }
                 }
-                if(array_key_exists('folderViews', $this->settings))
-                {
-                  if(file_exists($class) && file_exists($this->settings['folderViews'].$view))
-                  {
-                    require_once($class);
-                    require_once($this->settings['folderViews'].$view);
-                  }else
-                  {
-                    throw new \Exception("Class or view not found (custom)", 1);
-                  }
-                }
-                if(array_key_exists('folderViews', $this->settings) && array_key_exists('folderControllers', $this->settings))
-                {
-                  if(file_exists($this->settings['folderControllers'].$class) && file_exists($this->settings['folderViews'].$view))
-                  {
-                    require_once($this->settings['folderControllers'].$class);
-                    require_once($this->settings['folderViews'].$view);
-                  }else
-                  {
-                    throw new \Exception("Class or view not found (custom)", 1);
-                  }
-                }
-
-
               }
           }
     }
